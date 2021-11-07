3aff4efcc252deed914e4425c7cf3a166a743d2a
thanglq <luuthang2106@gmail.com>
2021-11-07 16:36:25 +0700

    add NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '@styles/globals.scss'
+import 'nprogress/nprogress.css'
 import type { AppProps } from 'next/app'
-
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const start = () => NProgress.start()
+    const stop = () => NProgress.done()
+    router.events.on('routeChangeStart', start)
+    router.events.on("routeChangeComplete", stop);
+    router.events.on("routeChangeError", stop);
+    return () => {
+      router.events.off('routeChangeStart', start)
+      router.events.off("routeChangeComplete", stop);
+      router.events.off("routeChangeError", stop);
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
